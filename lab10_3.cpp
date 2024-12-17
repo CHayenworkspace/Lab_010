ce8b4f4d95580e0ed0bc94db1c437b6898732461
CHayen030 <chayenlol268@gmail.com>
2024-12-17 16:24:05 +0700

    finished

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cmath>
+#include <iomanip>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("score.txt");
+	string text;
+	double sum ,sum_2 , i = 0;
+	while(getline(source,text))
+	{
+	    sum += stod(text) ;
+	    sum_2 += pow(stod(text),2) ;
+	    i++ ;
+	    
+	}
+	double mean = 1/i*sum;
+	double Standard = sqrt(((1/i)*sum_2)-pow(mean,2));
+    cout << "Number of data = " << i << "\n" ;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = "<< Standard;
+    source.close();
+}
